4dfdd1994e143ed3a5b41f0e4a067f9076802a8d
Dabronzo <ayrtonterceiro@gmail.com>
2021-12-07 21:36:30 +0000

    deleted most of the js to do it again

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,26 +5,35 @@ document.addEventListener("DOMContentLoaded", function(){
         stage.innerHTML = '<i class="fas fa-lock"></i>'
     }
 
-    let buttons = document.getElementsByTagName('button');
-    for (let button of buttons){
-        button.addEventListener('click', function(){
-            if (this.getAttribute('data-type') === 'play'){
-                startTheGame();
-            } 
-        })
-    }
+    let startButton = document.getElementById('start');
+    startButton.addEventListener('click', function(){
+        setCurrentStage('music');
+
+    })
+    
+        
 
 })
 
-function startTheGame (){
-    let pinkContainer = document.getElementById('enter-data');
-    pinkContainer.style.width = "40rem";
-    pinkContainer.innerHTML = `
-    <p class="animated">Tutorial</p>
-    <p class="tutorial animated">There are 4 thematic stages <i class="fas fa-music"></i> <i class="fas fa-film"></i> <i class="fas fa-atlas"></i> <i class="fas fa-skull-crossbones"></i></p>
-    <p class="tutorial animated">Each wrong question consumes 1 life</p>
-    <p class="tutorial animated">The multiple choices will be displayed bellow</p>
-    <P class="tutorial animated">Good Luck!</P>
-    <button class="tutorial-btn animated">Next</button>`
 
+
+
+
+function getQuestionByType (type){
+    let questionArray = [];
+    const allQuestions =[
+        {question: "what instrument did <strong>Paul Mccartney</strong> play on The Beatles?", type: "music", answers: ["bass", "keyboard", "drums", "banjo"], rightAnswer: "d" },
+        {question: "Which one of the following musicians used to be a <strong>teacher?</strong>", type: "music", answers: ["Michel Jackson", "Dave Grohl", "Sting", "Eminen"], rightAnswer: "b"},
+        {question: "Which country the band <strong>AC/DC</strong> is from?", type: "music", answers: ["USA", "England", "South Africa", "Australia"], rightAnswer: "a"},
+        {question: "How many oscars did the movie <strong>Titanic</strong> win", type: "movies", answers: ["21", "11", "14", "9"], rightAnswer: "c"},
+        {question: "Where were The Lord of the Rings movies filmed?", type: "movies", answers: ["Ireland", "Iceland", "New Zealand", "Australia"], rightAnswer: "b"},
+        {question: "what is the <strong>second</strong> rule of fight club", type: "movies", answers: ["don't talk about the fight club", "don't punch on the balls", "if is your 1st night you fight", "every fight is to death" ], rightAnswer: "a"}
+    ];
+
+    for (let question of allQuestions){
+        if (question.type === type){
+            questionArray.push(question)
+        }
+    }
+    return questionArray; 
 }
