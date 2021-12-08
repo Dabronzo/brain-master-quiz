e002d74341f9541edd2e60fbe6b316281f45aa43
Dabronzo <ayrtonterceiro@gmail.com>
2021-12-08 17:25:30 +0000

    added the new game logic on js

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,19 +4,100 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let stage of allStageIcons){
         stage.innerHTML = '<i class="fas fa-lock"></i>'
     }
+    let currentStage = "music";
 
-    let startButton = document.getElementById('start');
-    startButton.addEventListener('click', function(){
-        setCurrentStage('music');
-
-    })
-    
+    let startB = document.getElementById("start");
+    startB.addEventListener('click', function(){
+        console.log('starting the game');
+        theGame();
+    });
         
 
 })
 
+function theGame(){
+    let buttons = document.getElementsByClassName('info-answer-box');
+    let currStage = "music";
+    let lifes = 3;
+    console.log(lifes, "before");
+    let counter = 0;
+    let questList = getQuestionByType(currStage);
+    udpatePlayeStats();
+    displayQuestons(questList, counter);
+    for(let button of buttons){
+        button.addEventListener('click', function(){
+            if (this.getAttribute('data-type') === questList[counter].rightAnswer){
+                console.log("got it right");
+                updateCounter(updateQuest, updateStage);
+            } else {
+                lifes --;
+                console.log(lifes, "after");
+                updateLifes();
+                updateCounter(updateQuest, updateStage);
+            }
+        })
+    }
+
+    function updateCounter(callBack, callBackStage){
+        counter ++;
+        if (counter >= 3){
+            callBackStage()
+        } else{
+            callBack()
+        }
+
+    }
+
+    function updateQuest(){
+        displayQuestons(questList, counter);
+    }
+
+    function updateStage(){
+        counter = 0;
+        console.log('here the stage is updated');
+        if(currStage === "music"){
+            currStage = "movies";
+            questList = getQuestionByType(currStage);
+        } else{
+            console.log('end');
+        }
+        displayQuestons(questList, counter);
+    }
+
+    function updateLifes(){
+        document.getElementById('lifes').innerHTML = "";
+        for (i=0; i < lifes;i++){
+            document.getElementById('lifes').innerHTML += `<i class="fas fa-heart"></i>`;
+        }
+        checkAlive();
+    }
+
+    function checkAlive(){
+        if (lifes === 0){
+            alert('You are Dead');
+        }
+    }
+
+    function udpatePlayeStats(){
+        document.getElementById("player-status").classList.remove('hidden');
+        let name = document.getElementById('player-name').value;
+        document.getElementById('player').innerHTML = name;
+        updateLifes();
+    }
 
 
+
+}
+
+function displayQuestons(questions, indx){
+    let questArea = document.getElementById('enter-data');
+    let btnAnswers = document.getElementsByClassName('info-answer-box');
+    questArea.innerHTML = `<p>${questions[indx].question}</p>`;
+    for(let i=0;i < btnAnswers.length; i++){
+        btnAnswers[i].innerHTML = `<h4>${questions[indx].answers[i]}</h4>`;
+    }
+
+}
 
 
 function getQuestionByType (type){
